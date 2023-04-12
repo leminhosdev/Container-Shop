19f0e42748802a061aa0e48e7e64f2e6d5bea196
leminhosdev <lucaslemos8294@gmail.com>
2023-04-12 16:18:43 -0300

    re-starting spring security

---
+++ src/main/java/br/com/lucas/Container/Store/entity/ClienteUserDetails.java
@@ -0,0 +1,50 @@
+package br.com.lucas.Container.Store.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class ClienteUserDetails implements UserDetails {
+
+    private Cliente cliente;
+
+    public ClienteUserDetails(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return cliente.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return cliente.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
